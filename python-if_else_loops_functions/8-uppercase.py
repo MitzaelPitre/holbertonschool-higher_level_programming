379d0749f4f71d146a971c40d45b36029ffca4b3
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-01-27 23:50:25 -0400

    Coding

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+
 def uppercase(str):
     for i in range(len(str)):
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
             num = 32
         else:
             print("{}".format(chr(ord(str[i]) - num)), end='')
-            
-        print()
+    print() 
