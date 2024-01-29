a0f0747713d5515552e4b4e0fdf1bee55c635013
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-01-28 20:23:12 -0400

    Coding

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,11 +1,9 @@
-#!/usr/bin/python3
+vim #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Rellenar las tuplas con ceros si tienen menos de 2 elementos
     tuple_a = tuple_a + (0, 0) if len(tuple_a) < 2 else tuple_a[:2]
     tuple_b = tuple_b + (0, 0) if len(tuple_b) < 2 else tuple_b[:2]
 
-    # Sumar los elementos correspondientes de las tuplas
     sum1 = tuple_a[0] + tuple_b[0]
     sum2 = tuple_a[1] + tuple_b[1]
 
@@ -19,3 +17,6 @@ if __name__ == "__main__":
 
     print(add_tuple(tuple_a, (1, )))
     print(add_tuple(tuple_a, ()))
+
+    print(add_tuple((1,), (2, 3)))  # (3, 3)
+    print(add_tuple((1, 2, 3), (4, 5, 6)))  # (5, 7)
