97a1a4ec28111ecc94090f79e68ab24c8524e774
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-01-28 20:20:52 -0400

    Coding

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a = validate_tuple(tuple_a)
-    tuple_b = validate_tuple(tuple_b)
+    # Rellenar las tuplas con ceros si tienen menos de 2 elementos
+    tuple_a = tuple_a + (0, 0) if len(tuple_a) < 2 else tuple_a[:2]
+    tuple_b = tuple_b + (0, 0) if len(tuple_b) < 2 else tuple_b[:2]
 
-    return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+    # Sumar los elementos correspondientes de las tuplas
+    sum1 = tuple_a[0] + tuple_b[0]
+    sum2 = tuple_a[1] + tuple_b[1]
 
-def validate_tuple(_tuple=()):
-    if len(_tuple) < 2:
-        if len(_tuple) == 1:
-            _tuple = (_tuple[0], 0)
-        elif len(_tuple) == 0:
-            _tuple = (0, 0)
-    elif len(_tuple) > 2:
-        _tuple = (_tuple[0], _tuple[1])
+    return (sum1, sum2)
 
-    return _tuple
+if __name__ == "__main__":
+    tuple_a = (1, 89)
+    tuple_b = (88, 11)
+    new_tuple = add_tuple(tuple_a, tuple_b)
+    print(new_tuple)
 
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
+    print(add_tuple(tuple_a, (1, )))
+    print(add_tuple(tuple_a, ()))
