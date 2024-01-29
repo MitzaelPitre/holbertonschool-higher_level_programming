07888679f373f013df8b6e2cfd194eef52ac4422
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-01-28 20:28:18 -0400

    Coding

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,10 +4,9 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
 
-    max_n = min(my_list)
+    max_n = my_list[0]
     for i in my_list:
         if i > max_n:
-            max_n = 1
-
+            max_n = i
 
     return max_n
