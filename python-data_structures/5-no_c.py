6ce37c3446e58b5f34db6733fd0179132ce22797
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-01-28 20:31:35 -0400

    Coding

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -7,8 +7,3 @@ def no_c(mystring):
             new_str += i
 
     return new_str
-
-if __name__ == "__main__":
-    print(no_c("Best School"))
-    print(no_c("Chicago"))
-    print(no_c("C is fun!"))
