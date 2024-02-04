4e71a5bfec6cf22199ebe35428e84b22fdcc22c5
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-02-03 22:05:07 -0400

    Coding

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -3,9 +3,9 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        value_as_integer = int(value)  # Intenta convertir el valor en un entero
-        print(value_as_integer)  # Imprime el valor entero
+        value_as_integer = int(value)
+        print(value_as_integer)
         return True
     except ValueError:
-        sys.stderr.write("Exception: {} is not an integer\n".format(value))  # Maneja el error
+        sys.stderr.write("Exception: {} is not an integer\n".format(value))
         return False
