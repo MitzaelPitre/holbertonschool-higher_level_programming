8933dfc7cb3bde6f113d00bf744a0c9dc11c43e5
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-02-03 22:00:01 -0400

    Coding

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for item in my_list:
+    for item in my_list[:x]:  # Solo considera los primeros x elementos de la lista
         try:
             if isinstance(item, int):
                 print("{:d}".format(item), end='')
