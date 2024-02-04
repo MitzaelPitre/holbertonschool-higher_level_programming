bb2e0cfd0ccdec10735f7aefbc8e2ad13bb69e34
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-02-03 22:08:24 -0400

    Coding

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-
-def safe_print_integer_err(value):
-    try:
-        value_as_integer = int(value)
-        print(value_as_integer)
-        return True
-    except ValueError:
-        sys.stderr.write("Exception: {} is not an integer\n".format(value))
-        return False
+def safe_print_list_integers(my_list=[], x=0):
+    y = 0
+    for i in range(x):
+        try:
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end='')
+                y += 1
+        except IndexError:
+            break
+    print()
+    return y
