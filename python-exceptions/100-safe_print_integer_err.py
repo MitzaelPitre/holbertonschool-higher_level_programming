f4523734441c031885ca18eccff542a40357a0bd
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-02-03 21:48:43 -0400

    Coding

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
+        return True
     except (ValueError, TypeError) as e:
-        sys.stderr.write("Excpetion: {}\n".format(e))
+        sys.stderr.write("Exception: {}\n".format(e))
         return False
-    return True
--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 import sys
 
-
 def safe_function(fct, *args):
-    result = None
     try:
         result = fct(*args)
+        return result
     except Exception as e:
         sys.stderr.write("Exception: {}\n".format(e))
-        return result
+        return None
