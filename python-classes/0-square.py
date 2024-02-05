0cf4bce203d8a1439120e8035d9e35f0eb0f7734
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-02-04 21:17:38 -0400

    Coding

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""Square module, This module contains a class that defines a square.
+"""Square module.
+
+This module contains a class that defines a square.
 """
 
 class Square:
-    """class defines a square
-    """
+    """Defines a square."""
     
-    pass
+    def __init__(self):
+        pass
