f6e23690c36eec1d10f602bd169813d5b538a830
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-02-04 21:06:34 -0400

    Coding

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -6,5 +6,11 @@ This module contains a class that defines a square.
 
 class Square:
     """Defines a square."""
-
     pass
+
+# Create an instance of the Square class
+my_square = Square()
+
+# Print the type of my_square and its dictionary of attributes
+print(type(my_square))
+print(my_square.__dict__)
