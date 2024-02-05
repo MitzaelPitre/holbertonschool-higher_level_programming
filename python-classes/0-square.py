10cf447849c9776afe826ceee044c6269b45b32b
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-02-04 21:07:41 -0400

    Coding

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -8,9 +8,3 @@ class Square:
     """Defines a square."""
     pass
 
-# Create an instance of the Square class
-my_square = Square()
-
-# Print the type of my_square and its dictionary of attributes
-print(type(my_square))
-print(my_square.__dict__)
