dd2b995d11bf902eec8a27a38e56b47a3736e979
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-03-20 14:26:29 -0400

    Coding

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,15 +1,4 @@
-#!/bin/bash
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <database_name>"
-    exit 1
-fi
-
-read -p "Enter MySQL username: " mysql_user
-read -sp "Enter MySQL password: " mysql_password
-echo
-
-mysql -h localhost -u "$mysql_user" -p"$mysql_password" -D "$1" << EOF
-INSERT INTO first_table (id, name) VALUES (89, 'Best School');
-EOF
-
+-- 0x0D. SQL - Introduction, task 7. First add
+-- Inserts new row into `first_table`, DB passed as script arg.
+INSERT INTO first_table (id, name)
+       VALUES (89, 'Holberton School');
