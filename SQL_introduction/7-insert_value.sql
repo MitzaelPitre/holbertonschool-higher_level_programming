d196ec841a99bd721f9d4a5ecb65e0f190a237a6
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-03-20 14:24:56 -0400

    Coding

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,15 @@
--- Insert values
--- Execute: cat 7-insert_value.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <database_name>"
+    exit 1
+fi
+
+read -p "Enter MySQL username: " mysql_user
+read -sp "Enter MySQL password: " mysql_password
+echo
+
+mysql -h localhost -u "$mysql_user" -p"$mysql_password" -D "$1" << EOF
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+EOF
+
